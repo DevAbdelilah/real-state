6d4713b8a808d12a4961369113e12f5fbf5ea4ef
DevAbdelilah <charafiabdelilah3@gmail.com>
2025-04-14 22:58:35 +0100

    add landing page

---
+++ src/app/(nonedashboard)/landing/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <main className="bg-red "></main>
+    </div>
+  );
+};
+
+export default Layout;
